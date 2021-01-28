88c9e76dea4bdd73afbc3a5fa2bdcfcf67ad56a7
matheusleal-codem <alanwalkker.123@gmail.com>
2021-01-28 18:47:56 -0300

    display component

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Button from '../components/Button'
+import Display from '../components/Display'
 import './Calculator.css'
 
 export default () => {
     return(
         <div className="calculator">
+            <Display value={100}/>
             <Button label="AC"/>
             <Button label="/"/>
             <Button label="7"/>
