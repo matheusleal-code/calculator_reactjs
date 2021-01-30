6d3bb92a9a0375aa8a12d7fd9965954ee47de82a
matheusleal-codem <alanwalkker.123@gmail.com>
2021-01-30 16:18:42 -0300

    implementing the logic

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,50 +1,78 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Button from '../components/Button'
 import Display from '../components/Display'
 import './Calculator.css'
 
+const INITIAL_STATE = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
 
-    constructor(props){
+    state = { ...INITIAL_STATE }
+
+    constructor(props) {
         super(props)
         this.clearMemory = this.clearMemory.bind(this)
         this.setOperation = this.setOperation.bind(this)
         this.addDigit = this.addDigit.bind(this)
     }
 
-    clearMemory(){
-        console.log('limpar')
+    clearMemory() {
+        this.setState({ ...INITIAL_STATE })
     }
 
-    setOperation(operation){
+    setOperation(operation) {
         console.log(operation)
     }
 
-    addDigit(n){
-        console.log(n)
+    addDigit(n) {
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="calculator">
-                <Display value={100}/>
-                <Button label="AC" click={this.learMemory}/>
-                <Button label="/" click={this.setOperation}/>
-                <Button label="7" click={this.addDigit}/>
-                <Button label="8" click={this.addDigit}/>
-                <Button label="9" click={this.addDigit}/>
-                <Button label="*" click={this.setOperation}/>
-                <Button label="4" click={this.addDigit}/>
-                <Button label="5" click={this.addDigit}/>
-                <Button label="6" click={this.addDigit}/>
-                <Button label="-" click={this.setOperation}/>
-                <Button label="1" click={this.addDigit}/>
-                <Button label="2" click={this.addDigit}/>
-                <Button label="3" click={this.addDigit}/>
-                <Button label="+" click={this.setOperation}/>
-                <Button label="0" click={this.addDigit}/>
-                <Button label="."  click={this.addDigit}/>
-                <Button label="="  click={this.setOperation}/>
+                <Display value={this.state.displayValue} />
+                <Button label="AC" click={this.clearMemory} />
+                <Button label="/" click={this.setOperation} />
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" click={this.setOperation} />
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" click={this.setOperation} />
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" click={this.setOperation} />
+                <Button label="0" click={this.addDigit} />
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOperation} />
             </div>
         )
     }
